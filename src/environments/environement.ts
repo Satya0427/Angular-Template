48eca7b975ebf6395a6ee2b1ec251dea247becef
Satya0427 <satyamanikanta274@gmail.com>
2025-12-16 11:02:32 +0530

    2nd commit

--- src/environments/environement.ts
+++ src/environments/environement.ts
@@ -1,5 +1,4 @@
 export class Environment {
   public static production: boolean = false;
-  // public static baseApiUrl: string = 'https://atlqore.com/hrmswebapi';
   public static baseApiUrl: string = 'http://localhost:4000/api';
 }
